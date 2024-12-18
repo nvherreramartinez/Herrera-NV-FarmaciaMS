d655e553734f684e072df8ff915273f781f9739c
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-18 11:21:02 -0300

    Update CartView.jsx

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { useCart } from "../context/CartContext";
-import EmpyCart from './EmpyCart'
-import CartList from './CartList'
+//import React from "react";
+//import { useCart } from "../context/CartContext";
+//import EmpyCart from './EmpyCart'
+//import CartList from './CartList'
 
-const CartView = ()=> {
-    const {cart}= useCart()
-    return (
-        <div>
-            {!cart.lenght
-            ? <EmpyCart/>
-            : <div>
-                <h2>Tu carrito</h2>
-                <CartList/>
-            </div>
-            }
-        </div>
-    )
-}
-export default CartView
+//const CartView = ()=> {
+//    const {cart}= useCart()
+//    return (
+//       <div>
+ //           {!cart.lenght
+ //           ? <EmpyCart/>
+ //           : <div>
+ //               <h2>Tu carrito</h2>
+ //               <CartList/>
+ //           </div>
+ //           }
+ //       </div>
+//    )
+//}
+//export default CartView
