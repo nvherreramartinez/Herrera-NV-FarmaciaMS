7e5b9b941c915581a47706863cdd20207a12e838
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-18 11:25:36 -0300

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Item = ({producto}) => {
     return (
-        <div className="card" style={{width:'18rem', marginTop:10}}>
+        <div className="card-container">
             <img src={producto.img} className="card-img-top" alt={producto.name}/>
             <div className="card-body">
-                <h5 className='card-title'>{producto.name}</h5>
-                <p className='card-text'>${producto.price},00</p>
-                <p className='card-text'>Stock:{producto.stock} unidades.</p>
+                <h5 className='card-name'>{producto.name}</h5>
+                <p className='card-price'>${producto.price},00</p>
+                <p className='card-cantidad'>Stock:{producto.stock} unidades.</p>
                 <Link to={`/item/${producto.id}`} className="btn btn-dark">Ver más</Link>
             </div>
         </div>
