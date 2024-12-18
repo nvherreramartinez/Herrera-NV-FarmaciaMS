864195cbac73ffd67462726fd5ede789276375dd
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-18 11:21:53 -0300

    Update NavbarComponent.jsx

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import CartWidget from "./CarWidget";
-import { NavLink } from "react-router-dom";
+//import React from "react";
+//import CartWidget from "./CarWidget";
+//import { NavLink } from "react-router-dom";
 
-const NavbarComponent = () =>{
-    return(
-        <nav className="navContainer">
-            <NavLink className="aLink" to='/'>Farmacia MS</NavLink>
-            <NavLink className="aLink" to='/producto/laboratorios'>Laboratorios</NavLink>
-            <NavLink className="aLink" to='/producto/Ofertas'>Ofertas</NavLink>
-            <NavLink className="aLink" to='/producto/obra sociales'>Obras Sociales</NavLink>
-            <NavLink className='aLink' to='/cart'><CartWidget counter={15}/></NavLink>
-        </nav>
-    )
-}
+//const NavbarComponent = () =>{
+//    return(
+  //      <nav className="navContainer">
+ //           <NavLink className="aLink" to='/'>Farmacia MS</NavLink>
+ //           <NavLink className="aLink" to='/producto/laboratorios'>Laboratorios</NavLink>
+ //           <NavLink className="aLink" to='/producto/Ofertas'>Ofertas</NavLink>
+ //           <NavLink className="aLink" to='/producto/obra sociales'>Obras Sociales</NavLink>
+//            <NavLink className='aLink' to='/cart'><CartWidget counter={15}/></NavLink>
+//        </nav>
+//    )
+//}
 
-export default NavbarComponent
+//export default NavbarComponent
