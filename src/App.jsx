bebff3e4c383ca1a7c160d80f98b3fefa9f65198
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-19 17:25:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import NavbarReactBootstrap from './components/NavbarReactBoostrap'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Error from './components/Error'
 import { CartProvider } from './context/CartContext'
-import Loader from './components/Loader'
 import CartContainer from './components/CartContainer'
 import Checkout from './components/Checkout'
 
@@ -19,10 +18,9 @@ function App() {
                 <NavbarReactBootstrap/>
                 <Routes>
                     <Route path='/' element={<ItemListContainer greeting='Bienvenidos a la Farmacia del Ministerio!'/>}/>
-                    <Route path='/producto/:categoryId' element={<ItemListContainer greeting='Bienvenidos a la Farmacia del Ministerio!'/>}/>
+                    <Route path='category/:categoryId' element={<ItemListContainer greeting='Bienvenidos a la Farmacia del Ministerio!'/>}/>
                     <Route path='/item/:id' element={<ItemDetailContainer/>}/>
-                    <Route path='/cart' element={<CartContainer/>}/>
-                    <Route path='/loading' element={<Loader/>}/>
+                    <Route path='/:cart' element={<CartContainer/>}/>
                     <Route path='/checkout' element={<Checkout/>}/>
                     <Route path='*' element={<Error/>}/>
                 </Routes>
