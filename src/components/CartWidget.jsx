077e0537f70b4966d46f47717332096015bfdf64
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-19 17:18:21 -0300

    Update CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,6 +3,7 @@ import { Badge } from 'react-bootstrap';
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 
+
 const CartWidget = () => {
     const {cart} = useContext(CartContext)
     const {cartQuantity} = useContext(CartContext)
@@ -11,8 +12,10 @@ const CartWidget = () => {
         <div className='CartWidgetContainer'>
             <IoCart fontSize={'1.8rem'} color="white" />
             {cartQuantity() > 0 && <Badge bg="info">{cartQuantity()}</Badge>}
+            
         </div>
     )
 };
 
 export default CartWidget
+
