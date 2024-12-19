5916d95ca30a37ad29ccb0313b6ecfea91739bc6
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-19 17:29:11 -0300

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Error = () => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 15 }}>
-            <img src="https://img.freepik.com/vector-gratis/fondo-error-404-diseno-divertido_1167-219.jpg?w=2000" alt='error' style={{ width: '90%', marginBottom: 15 }} />
+            <img src="https://i.postimg.cc/SK5Q1bH1/error-404.png" alt='error' style={{ width: '90%', marginBottom: 15 }} />
             <Link className='btn btn-dark' to='/'>Volver a Home</Link>
         </div>
     )
