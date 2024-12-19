a9c7e63f5f748bb1004c6f48a13210a57334c203
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-19 17:17:28 -0300

    Update CartContainer.jsx

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -2,20 +2,20 @@ import React, { useContext } from "react"
 import { CartContext } from "../context/CartContext"
 import CartItem from "./CartItem"
 import EmpyCart from "./EmpyCart"
+import { Link } from "react-router-dom"
 
 const CartContainer = () => {
-    const{cart} = useContext(CartContext)
+    const{cart, cartTotal, clear} = useContext(CartContext)
     return (
         <div>
-        {!cart.lenght
-            ? <EmpyCart/>
-            : <div>
+        {!cart.length ? <EmpyCart/>
+        : <div className="cartWidgetContainer">
                 <h2>Tu carrito</h2>
                 {cart.map((producto) => <CartItem key={producto.id} producto={producto}/>)}
                 <span>Monto total de su compra: ${cartTotal()}</span>
-                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", padding: "2rem"}}>
+                <div>
                     <button className="btn btn-danger" onClick={clear}>Borrar carrito</button>
-                    <button className="btn btn-success">Finalizar su compra</button>
+                    <Link className='btn btn-success' to='/checkout'>Finalizar Compra</Link>
                 </div>
             </div>
             }
