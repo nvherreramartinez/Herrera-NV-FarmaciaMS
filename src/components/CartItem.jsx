0b534b61ff5c3812baa9e6d00982027f56ed5e20
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2024-12-19 17:17:50 -0300

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,9 +4,9 @@ import { CartContext } from "../context/CartContext";
 const CartItem = ({producto}) => {
     const {removeItem} = useContext(CartContext)
     return (
-        <div className="card-item">
+        <div className="card-check">
             <img className="card-img" src = {producto.img} alt = {producto.name}/>
-            <span className="card-name">{producto.name} style={{width:'10rem'}}</span>
+            <span className="card-name">{producto.name}</span>
             <span className="card-cantidad">Unidades: {producto.cantidad}</span>
             <span className="card-price">${producto.price},00</span>
             <span className="card-final-price">Precio final: ${producto.cantidad * producto.price},00</span>
